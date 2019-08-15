75b0858b9261b112d12b20479b3749dc95f2decc
swicksu <swick3415@gmail.com>
2019-08-15 16:14:17 +0800

    添加Mgr

---
+++ Unity/Assets/Hotfix/Module/Scene/SceneMgr.cs
@@ -0,0 +1,18 @@
+﻿using ETModel;
+
+namespace ETHotfix
+{
+	public static class SceneMgr
+	{
+		public static async void LoadScene(string sceneName)
+		{
+			// 加载场景资源
+			await ETModel.Game.Scene.GetComponent<ResourcesComponent>().LoadBundleAsync(sceneName.StringToAB());
+			// 切换到map场景
+			using (SceneChangeComponent sceneChangeComponent = ETModel.Game.Scene.AddComponent<SceneChangeComponent>())
+			{
+				await sceneChangeComponent.ChangeSceneAsync(sceneName);
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Hotfix/Module/UI/UIMgr.cs
@@ -0,0 +1,24 @@
+﻿using ETModel;
+
+namespace ETHotfix
+{
+	public static class UIMgr
+	{
+		public static void OpenPanel<T>(string uiName) where T : Component, new()
+		{
+			UI ui = UIFactory.Create<T>(uiName);
+			Game.Scene.GetComponent<UIComponent>().Add(ui);
+		}
+
+		public static T GetPanel<T>(string uiName) where T : Component, new()
+		{
+			return Game.Scene.GetComponent<UIComponent>().Get(uiName).GetComponent<T>();
+		}
+
+		public static void ClosePanel(string uiName)
+		{
+			Game.Scene.GetComponent<UIComponent>().Remove(uiName);
+			ETModel.Game.Scene.GetComponent<ResourcesComponent>().UnloadBundle(uiName.StringToAB());
+		}
+	}
+}
