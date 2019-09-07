76987429f0c4d7c06fc213d434842cdda2d192c2
swicksu <swick34151@gmail.com>
2019-09-07 22:12:13 +0800

    增加游戏对象池

---
+++ Unity/Assets/Hotfix/Module/Plus/Pool/GameObjectPool.cs
@@ -0,0 +1,62 @@
+﻿using ETModel;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ETHotfix
+{
+	public static class GameObjectPool
+	{
+		private class GOInfo
+		{
+			public string name;
+			public GameObject prefab;
+		}
+
+		private static Dictionary<GOInfo, Queue<GameObject>> goTable = new Dictionary<GOInfo, Queue<GameObject>>();
+
+		public static GameObject Allocate(string name)
+		{
+			GOInfo info = GetGOInfo(name);
+			Queue<GameObject> queue = goTable[info];
+			if (queue.Count > 0)
+			{
+				return queue.Dequeue().Enable();
+			}
+			else
+			{
+				return info.prefab.Instantiate();
+			}
+		}
+
+		public static void Recycle(string name, GameObject go)
+		{
+			if (go)
+			{
+				go.Disable();
+				GOInfo info = GetGOInfo(name);
+				goTable[info].Enqueue(go);
+			}
+		}
+
+		private static GOInfo GetGOInfo(string name)
+		{
+			var iter = goTable.GetEnumerator();
+			while (iter.MoveNext())
+			{
+				GOInfo info = iter.Current.Key;
+				if (info.name == name)
+				{
+					return info;
+				}
+			}
+
+			// 没有就创建
+			GOInfo goInfo = new GOInfo() { name = name, prefab = ResourceUtil.Load<GameObject>(name) };
+			Queue<GameObject> queue = new Queue<GameObject>();
+			goTable.Add(goInfo, queue);
+			return goInfo;
+		}
+	}
+}
